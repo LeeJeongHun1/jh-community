9f169fcd808deeebd2099e32b32099a3e909052c
JHLEE\ADMIN <wjdgns1155@naver.com>
2022-05-20 01:01:19 +0900

    Board VO, controller 수정

--- zaritalk/src/main/java/kr/co/zaritalk/controller/ZaritalkController.java
+++ zaritalk/src/main/java/kr/co/zaritalk/controller/ZaritalkController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,8 @@ import kr.co.zaritalk.vo.User;
 
 /**
  * @author "jhlee"
- *
+ * 커뮤니티 게시판 컨트롤러 
+ * 게시글 목록, 조회, 수정, 삭제 RestAPI
  */
 @RestController
 @RequestMapping("/boards")
@@ -34,24 +36,30 @@ public class ZaritalkController {
 	 * @return 게시글 목록
 	 */
 	@GetMapping()
-	@ApiOperation(notes = "커뮤니티 전체 게시글을 조회 할 수 있습니다.", value = "목록조회")
+	@ApiOperation(notes = "커뮤니티 게시글 목록을 조회 할 수 있습니다.", value = "목록조회")
 	public List<Board> boards() {
 		return service.selectList();
 	}
 	
-	@PostMapping("/{seq}")
-	@ApiOperation(notes = "커뮤니티 전체 게시글을 조회 할 수 있습니다.", value = "유저조회")
+	@GetMapping("/{seq}")
+	@ApiOperation(notes = "커뮤니티 게시글을 조회 할 수 있습니다.", value = "게시글 조회")
 	@ApiImplicitParams({
 		@ApiImplicitParam(name = "seq", value = "id", example = "1")
 		
 	})
-	public List<Board> boards1(@PathVariable int seq) {
+	public List<Board> boards(@PathVariable int seq) {
 		return service.selectList();
 	}
-//	@GetMapping()
-//	public List<User> boards2() {
-//		return service.selectList();
-//	}
+	
+	@ApiOperation(notes = "커뮤니티 전체 게시글을 조회 할 수 있습니다.", value = "게시글 조회")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "seq", value = "id", example = "1")
+		
+	})
+	@PutMapping("/{seq}")
+	public int update(int seq) {
+		return 0;
+	}
 //	@GetMapping()
 //	public List<User> boards3() {
 //		return service.selectList();
--- zaritalk/src/main/java/kr/co/zaritalk/vo/Board.java
+++ zaritalk/src/main/java/kr/co/zaritalk/vo/Board.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 @Data
 public class Board {
+	private int seq;
 	private String title;
 	private String body;
 	private String nickname;
