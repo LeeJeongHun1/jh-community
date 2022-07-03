f242bf1179e2db5d9e27065475d2dc1555fd5ee7
JHLEE\ADMIN <wjdgns1155@naver.com>
2022-07-04 01:11:41 +0900

    페이지 기능 추가

--- communityJh/src/main/java/kr/co/communityJh/controller/BoardController.java
+++ communityJh/src/main/java/kr/co/communityJh/controller/BoardController.java
@@ -5,18 +5,22 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.communityJh.service.BoardService;
 import kr.co.communityJh.vo.Board;
-import kr.co.communityJh.vo.User;
 
 /**
  * @author "jhlee"
@@ -34,41 +38,57 @@ public class BoardController {
 	 * db에 조회된 모든 게시글을 보여준다. 추후 페이징, auth 설정 필요
 	 */
 	@GetMapping
-	public String boards(Model model) {
-		System.out.println("/ 접속");
-		model.addAttribute("boards", BoardService.findAll());
+	public String boards(Model model,
+			@PageableDefault(
+					page = 0,
+					size = 1,
+					sort = "createDate",
+					direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<Board> boards = BoardService.findAll(pageable);
+ 		int startPageNumber = BoardService.getStartPageNumber(boards.getPageable());
+		int endPageNumber = BoardService.getEndPageNumber(
+				boards.getPageable().getPageNumber(), boards.getTotalPages());
+		model.addAttribute("startPageNumber", startPageNumber);
+		model.addAttribute("endPageNumber", endPageNumber);
+		model.addAttribute("boards", boards);
 		return "board/list";
 		
 	}
 	/**
-	 * @return 게시판 list page
+	 * 게시글 seq에 해당하는 게시글 상세 페이지
 	 * db에 조회된 모든 게시글을 보여준다. 추후 페이징, auth 설정 필요
+	 * seq 값이 없을 경우 list 페이지로 리턴
+	 * @param seq
+	 * @return 게시판 상세 페이지
+	 * @throws Exception 
 	 */
-	@GetMapping({"/detail/{seq}", "/detail"})
-	public String detail(@PathVariable Optional<Integer> seq, Model model, @AuthenticationPrincipal User a) {
-//		System.out.println("detail 접속" + seq.get());
-//		System.out.println(seq.orElseThrow(() -> {
-//			return new BoardIdNotFoundException("해당 게시글을 찾을 수 없습니다." );
-//		}));
-//		model.addAttribute("boards", BoardService.findById());
-		if(seq.isPresent()) {
-			model.addAttribute("boards", BoardService.findById(seq.get()));
-		}else {
-			
+	@GetMapping("/{seq}")
+	public String detail(@PathVariable Optional<Integer> seq, Model model) throws IllegalArgumentException {
+		if(!seq.isPresent()) {
+			return "redirect:/boards";
 		}
+		model.addAttribute("board", BoardService.findById(seq.get())
+				.orElseThrow(() -> {
+					return new IllegalArgumentException("<h1>해당 게시글은 존재하지 않습니다!</h1>");
+				}));
 		return "board/detail";
 		
 	}
 	
+	/**
+	 * 게시글 작성 페이지
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/write")
 	public String write(Model model) {
-		System.out.println("write 접속");
 		model.addAttribute("board", new Board());
 		return "board/write";
 		
 	}
 	
 	/**
+	 * 게시글 등록
 	 * @param board
 	 * @return
 	 */
@@ -78,7 +98,23 @@ public class BoardController {
 			return "board/write";
 		}
 		BoardService.save(board);
-		return "redirect:/boards/detail/" + board.getSeq();
+		return "redirect:/boards/" + board.getId();
+		
+	}
+	
+	/**
+	 * 게시글 수정
+	 * @param board
+	 * @return
+	 */
+	@PutMapping("/write")
+	public String update(@Valid @RequestBody Board board,
+			BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "board/write";
+		}
+		BoardService.save(board);
+		return "redirect:/boards/detail/" + board.getId();
 		
 	}
 	
--- communityJh/src/main/java/kr/co/communityJh/service/BoardService.java
+++ communityJh/src/main/java/kr/co/communityJh/service/BoardService.java
@@ -1,9 +1,10 @@
 package kr.co.communityJh.service;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import kr.co.communityJh.repository.BoardRepository;
@@ -11,19 +12,41 @@ import kr.co.communityJh.vo.Board;
 
 @Service
 public class BoardService {
+	// 한번에 보여줄 페이지 수
+	private static final int PAGEABLE_SIZE = 5;
 	
 	@Autowired
 	private BoardRepository boardRepository;
 	
-	public List<Board> findAll() {
-		return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "createDate"));
+	// Slice 객체로 return 할 경우 다음 slice 존재 유무만 알고 있다.
+	public Page<Board> findAll(Pageable pageable) {
+//		boardRepository.findAll(PageRequest.of(0, 5, Sort.Direction.DESC, "createDate"));
+		return boardRepository.findAll(pageable);
 	}
 	
 	public void save(Board board) {
 		boardRepository.save(board);
 	}
 	
-	public Board findById(int id) {
-		return boardRepository.findById(id).get();
+	public Optional<Board> findById(int id) {
+		return boardRepository.findById(id);
+	}
+	
+	
+	// 0~4, 5~9, 10~14 -> 10
+	public int getStartPageNumber(Pageable pageable) {
+		int startPageNumber = pageable.getPageNumber() - (PAGEABLE_SIZE-1) <= 0 ?
+				0 : (pageable.getPageNumber() / PAGEABLE_SIZE) * PAGEABLE_SIZE;
+		return startPageNumber;
+	}
+	
+	// 0~4, 5~9, 10~14 
+	// total: 12, pageNumber: 9 -> 9
+	public int getEndPageNumber(int pageNumber, int totalPage) {
+		int EndPageNumber = (((pageNumber) / PAGEABLE_SIZE) +1) * PAGEABLE_SIZE;
+		if(totalPage < EndPageNumber) {
+			EndPageNumber = totalPage;
+		}
+		return EndPageNumber-1;
 	}
 }
